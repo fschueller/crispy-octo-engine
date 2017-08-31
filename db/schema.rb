8c99a835a261698e3992c98e601fcb3a5b2b4e00
fschueller <fschueller@suse.de>
2017-08-31 14:37:32 +0200

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709134004) do
+ActiveRecord::Schema.define(version: 20170831123555) do
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "avatar_url"
+    t.string "name"
     t.string "email"
-    t.string "uid"
-    t.string "provider"
-    t.string "oauth_token"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
