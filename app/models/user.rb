a89d65f1392a32806412945901e4ee2bdf32fcf1
fschueller <fschueller@suse.de>
2017-08-31 14:46:28 +0200

    Prepare for custom user auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :name, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL }, uniqueness: true
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :password, presence: true, length: { minimum: 8 }
+
+  before_save { self.email = email.downcase }
+
+  has_secure_password
 end
